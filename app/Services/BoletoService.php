07fa05c769b37c51ea6dfb201702d74c5697b5c6
Gabriel Caminha <gabrielcaminha255@gmail.com>
2025-06-26 15:34:32 -0300

    feat: adiciona variaveis de logica; importa Carbon

--- app/Services/BoletoService.php
+++ app/Services/BoletoService.php
@@ -2,9 +2,22 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
+
 class BoletoService
 {
     private const MULTA_PERCENTUAL = 0.02; // 2%
     private const JUROS_DIA = 0.00033; // 0.033% ao dia
     private const JUROS_MENSAL_PARCELAMENTO = 0.013; // 1.3% ao mês
+
+    private $valorOriginal;
+    private $vencimento;
+    private $dataPagamento;
+    private $parcelas = null;
+
+    private $diasAtrasado;
+
+    private $multa;
+    private $juros;
+    private $valorTotal;
 }
