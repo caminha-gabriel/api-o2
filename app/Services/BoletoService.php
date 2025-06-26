0ec83d640b9816053ccc7f4f8c848ff2b51d2a67
Gabriel Caminha <gabrielcaminha255@gmail.com>
2025-06-26 17:03:11 -0300

    feat: adiciona calculo de boleto junto a calculo especifico de parcelamento

--- app/Services/BoletoService.php
+++ app/Services/BoletoService.php
@@ -21,6 +21,39 @@ class BoletoService
     private float $juros;
     private float $valorTotal;
 
+    public function calcularBoleto(float $valor, string $vencimento, string $dataPagamento, ?int $parcelas = null) {
+        $this->valorOriginal = $valor;
+        $this->valorTotal = $valor;
+        $this->vencimento = Carbon::parse($vencimento);
+        $this->dataPagamento = Carbon::parse($dataPagamento);
+        $this->parcelas = $parcelas;
+
+        $this->calcularAtraso();
+
+        $resposta = [
+            "valor_original" => round($this->valorOriginal, 2),
+            "dias_em_atraso" => $this->diasAtrasado
+        ];
+
+        if ($this->diasAtrasado === 0) {
+            return $resposta;
+        }
+
+        $resposta += [
+            'multa' => round($this->multa, 2),
+            'juros' => round($this->juros, 2),
+            'valor_atualizado' => round($this->valorTotal, 2),
+        ];
+
+        if ($this->parcelas) {
+            $parcelamento = $this->calcularParcelamento();
+
+            $resposta = array_merge($resposta, $parcelamento);
+        }
+
+        return $resposta;
+    }
+
     private function calcularAtraso() {
         $this->diasAtrasado = max($this->vencimento->diffInDays($this->dataPagamento, false), 0);
 
@@ -37,5 +70,18 @@ class BoletoService
 
     private function aplicarJuros() {
         $this->juros = $this->valorTotal * self::JUROS_DIA * $this->diasAtrasado;
+    }
+
+    private function calcularParcelamento() {
+        $jurosTotal = $this->valorTotal * self::JUROS_MENSAL_PARCELAMENTO * $this->parcelas;
+        $valorTotalComJuros = $this->valorTotal + $jurosTotal;
+        $valorParcela = round($valorTotalComJuros / $this->parcelas, 2);
+
+        return [
+            'parcelas' => $this->parcelas,
+            'juros_parcelamento' => round($jurosTotal, 2),
+            'valor_total_com_juros' => round($valorTotalComJuros, 2),
+            'valor_parcela' => $valorParcela,
+        ];
     }
 }
